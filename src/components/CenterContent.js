1bfbf45e8f8c92d891fc65c2f8bb0d2047026f59
tfunk2 <tyler.j.funk2@gmail.com>
2021-03-03 03:16:17 -0700

    Added vertical name, and made responsive thus far

---
+++ src/components/CenterContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/CenterContent.css";
+
+const CenterContent = () => {
+  return (
+    <div className="center-content-div">
+      <h1 className="first-name">Tyler</h1>
+      <h1 className="last-name">Funk</h1>
+    </div>
+  );
+};
+
+export default CenterContent;
