59166cde446d27374e851765ca706fa4f718f76a
tfunk2 <tyler.j.funk2@gmail.com>
2021-03-03 23:32:44 -0700

    bottom nav added, all animations updated to play out as well as in on hover

---
+++ src/components/BottomNavigation.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+import '../stylesheets/BottomNavigation.css'
+
+export default function BottomNavigation() {
+  return (
+    <div class="bottom-nav-div">
+      <section className="bottom-rectangle-tab" id="tab-6"></section>
+      <section className="bottom-rectangle-tab" id="tab-7"></section>
+      <section className="bottom-rectangle-tab" id="tab-8"></section>
+      <section className="bottom-rectangle-tab" id="tab-9"></section>
+      <section className="bottom-rectangle-tab" id="tab-10"></section>
+    </div>
+  );
+}
