a477d13a542a330ab9de9f67b48bbb54d4fb42ef
tfunk2 <tyler.j.funk2@gmail.com>
2021-03-04 01:18:25 -0700

    Added top tab text and styling, needs to be more responsive for smaller sizes

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,14 +1,24 @@
-import React, {useState, useEffect} from "react";
-import '../stylesheets/TopNavigation.css'
+import React, { useState, useEffect } from "react";
+import "../stylesheets/TopNavigation.css";
 
 export default function TopNavigation() {
   return (
     <div class="top-nav-div">
-      <section className="rectangle-tab" id="tab-1"></section>
-      <section className="rectangle-tab" id="tab-2"></section>
-      <section className="rectangle-tab" id="tab-3"></section>
-      <section className="rectangle-tab" id="tab-4"></section>
-      <section className="rectangle-tab" id="tab-5"></section>
+      <section className="rectangle-tab" id="tab-1">
+        <span className="top-tab-name">The Real Kates Kitchen</span>
+      </section>
+      <section className="rectangle-tab" id="tab-2">
+        <span className="top-tab-name">Home</span>
+      </section>
+      <section className="rectangle-tab" id="tab-3">
+        <span className="top-tab-name">Funky Cafe</span>
+      </section>
+      <section className="rectangle-tab" id="tab-4">
+        <span className="top-tab-name">Resume</span>
+      </section>
+      <section className="rectangle-tab" id="tab-5">
+        <span className="top-tab-name">LinkedIn</span>
+      </section>
     </div>
   );
 }
