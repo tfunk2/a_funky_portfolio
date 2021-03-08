f3c8341a9c25d0b65ecbc879a31f55f0bb0e3621
tfunk2 <tyler.j.funk2@gmail.com>
2021-03-08 03:26:40 -0700

    Gave active tab capability to bottom nav to match top nav

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -6,35 +6,35 @@ export default function BottomNavigation({ activeTab, handleClick }) {
     <div class="bottom-nav-div">
       <section
         onClick={() => handleClick("Nalarama")}
-        className="bottom-rectangle-tab"
+        className={activeTab === "Nalarama" ? "bottom-rectangle-tab active-tab" : "bottom-rectangle-tab"}
         id="tab-6"
       >
         <span className="bottom-tab-name">Nalarama</span>
       </section>
       <section
         onClick={() => handleClick("Medium")}
-        className="bottom-rectangle-tab"
+        className={activeTab === "Medium" ? "bottom-rectangle-tab active-tab" : "bottom-rectangle-tab"}
         id="tab-7"
       >
         <span className="bottom-tab-name">Medium</span>
       </section>
       <section
         onClick={() => handleClick("GitHub")}
-        className="bottom-rectangle-tab"
+        className={activeTab === "GitHub" ? "bottom-rectangle-tab active-tab" : "bottom-rectangle-tab"}
         id="tab-8"
       >
         <span className="bottom-tab-name">GitHub</span>
       </section>
       <section
         onClick={() => handleClick("About Me")}
-        className="bottom-rectangle-tab"
+        className={activeTab === "About Me" ? "bottom-rectangle-tab active-tab" : "bottom-rectangle-tab"}
         id="tab-9"
       >
         <span className="bottom-tab-name">About Me</span>
       </section>
       <section
         onClick={() => handleClick("In Progress")}
-        className="bottom-rectangle-tab"
+        className={activeTab === "In Progress" ? "bottom-rectangle-tab active-tab" : "bottom-rectangle-tab"}
         id="tab-10"
       >
         <span className="bottom-tab-name">In Progress</span>
