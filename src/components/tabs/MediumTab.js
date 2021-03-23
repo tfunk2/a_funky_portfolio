a64d0a403fb7022591664c319e96c12fb1741269
tfunk2 <tyler.j.funk2@gmail.com>
2021-03-23 06:37:23 -0600

    Added blurb to MediumTab, fixed faded-scroll color issue for Safari

--- src/components/tabs/MediumTab.js
+++ src/components/tabs/MediumTab.js
@@ -12,14 +12,27 @@ const MediumTab = () => {
       <div className="center-body">
         <h2 className="center-title">Medium</h2>
         <div className="faded-scroll" id="custom-fade-4"></div>
-        <a target="_blank" className="medium-link" href="https://tyler-j-funk.medium.com/">
+        <a
+          target="_blank"
+          className="medium-link"
+          href="https://tyler-j-funk.medium.com/"
+        >
           <img className="medium-logo" src={mediumLogo} alt="medium logo"></img>
           <h4 className="medium-name">@tyler-j-funk</h4>
         </a>
+        <p className="center-p" id="medium-p">
+          On a semi-regular basis I write a technical blog post on Medium about
+          whatever I have been learning recently, or things I am interested in.
+          Sometimes I write about things that I relearned and didn't want to
+          forget again! I was lucky enough to have some publications reach out
+          to me, and I submitted some articles to publish with them. Check them
+          out below!
+        </p>
         <h3>Publications I Write For</h3>
         <div className="publications-div">
           {/* JavaScript in Plain English */}
-          <a target="_blank"
+          <a
+            target="_blank"
             className="publication-link"
             href="https://javascript.plainenglish.io/"
           >
@@ -31,7 +44,11 @@ const MediumTab = () => {
             <h5 className="publication-name">JavaScript In Plain English</h5>
           </a>
           {/* Dev Genius */}
-          <a target="_blank" className="publication-link" href="https://medium.com/dev-genius">
+          <a
+            target="_blank"
+            className="publication-link"
+            href="https://medium.com/dev-genius"
+          >
             <img
               className="dev-genius-logo"
               src={devGeniusLogo}
@@ -40,7 +57,8 @@ const MediumTab = () => {
             <h5 className="publication-name">Dev Genius</h5>
           </a>
           {/* Level Up Coding by gitconnected */}
-          <a target="_blank"
+          <a
+            target="_blank"
             className="publication-link"
             href="https://levelup.gitconnected.com/"
           >
@@ -54,7 +72,11 @@ const MediumTab = () => {
             </h5>
           </a>
           {/* The Startup */}
-          <a target="_blank" className="publication-link" href="https://medium.com/swlh">
+          <a
+            target="_blank"
+            className="publication-link"
+            href="https://medium.com/swlh"
+          >
             <img
               className="the-startup-logo"
               src={theStartupLogo}
@@ -63,52 +85,6 @@ const MediumTab = () => {
             <h5 className="publication-name">The Startup</h5>
           </a>
         </div>
-        <p className="center-p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a dolor
-          rutrum, vulputate sapien vel, condimentum est. Vestibulum cursus diam
-          varius ipsum pretium ullamcorper. Mauris diam lectus, posuere nec
-          risus a, tempor maximus augue. Donec accumsan massa ac ex venenatis,
-          et congue enim consequat. Vestibulum ante ipsum primis in faucibus
-          orci luctus et ultrices posuere cubilia curae; Duis aliquam felis nec
-          dictum ultrices. Quisque pretium mi id mi faucibus varius. Aenean non
-          erat nec lacus facilisis tincidunt. Duis velit sapien, ultricies in
-          cursus et, lacinia sed est. Suspendisse potenti. Aenean finibus,
-          lectus nec accumsan iaculis, diam augue blandit libero, ac sodales
-          purus augue eu lectus. Aenean blandit dui at ligula porttitor, vitae
-          pulvinar diam vehicula. Nulla pharetra libero quis turpis dignissim,
-          sit amet tempor dui mattis. Ut vel fringilla lectus, a fringilla
-          augue. Curabitur facilisis tincidunt erat, at fermentum arcu
-          sollicitudin vitae. Nulla et congue purus, ac rutrum mi. In vel ex vel
-          augue lobortis scelerisque eu ac ex. Maecenas faucibus, nisi sit amet
-          mollis posuere, nibh quam scelerisque libero, in vestibulum quam ante
-          non nisl. Fusce tincidunt a justo eu iaculis. Aenean eu magna vitae
-          dolor pulvinar blandit ut quis magna. Donec pretium lacus metus, vitae
-          ullamcorper est ultricies vitae. Cras a lacinia turpis, ac rhoncus
-          arcu. Curabitur a nunc commodo lorem dictum consequat. Nunc tincidunt
-          magna nisl, et sagittis purus sagittis at. Mauris fringilla sit amet
-          tortor vel tristique. Cras condimentum congue lectus quis dictum.
-          Aenean elit magna, rhoncus nec mattis nec, convallis a elit.
-          Suspendisse gravida a arcu vitae congue. Phasellus non vulputate
-          velit. In quam dolor, rutrum nec ligula sit amet, laoreet condimentum
-          diam. Maecenas quis est ut diam varius elementum id non sapien.
-          Praesent congue, metus quis vehicula accumsan, velit nisl malesuada
-          magna, congue efficitur turpis augue vitae libero. Sed ut interdum
-          sapien. Vestibulum eget imperdiet enim. Duis venenatis consequat augue
-          ac condimentum. Phasellus rutrum est nibh. Aliquam tincidunt massa
-          orci, ut mattis sapien fermentum vitae. Donec imperdiet lectus est,
-          vitae dapibus felis semper vitae. Fusce auctor odio neque, eget
-          lobortis lectus semper non. Pellentesque posuere augue rhoncus, congue
-          nibh in, sollicitudin odio. Donec quis nisl id orci dictum
-          condimentum. Cras tincidunt tempor urna, in rhoncus nibh consectetur
-          a. Integer quis pharetra risus. Vestibulum sit amet ante nec metus
-          sollicitudin malesuada. Vivamus purus sapien, aliquet eget mollis at,
-          finibus et velit. Donec turpis eros, pretium aliquam nisi a, tristique
-          aliquam risus. Phasellus ac nulla in quam venenatis blandit. Mauris
-          sollicitudin eleifend magna dapibus eleifend. Ut maximus, ligula eu
-          varius tincidunt, nibh nulla consequat dui, a sagittis eros risus nec
-          sem. Suspendisse sed elit aliquam, lobortis risus sagittis, euismod
-          erat. Ut suscipit nisi a justo tristique, sed sodales lorem efficitur.
-        </p>
       </div>
       <div className="faded-scroll-bottom" id="custom-fade-4-bottom"></div>
     </section>
